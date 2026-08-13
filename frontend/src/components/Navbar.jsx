01df38daf8ab688853b138f278d2ef14676c7a59
Praneeth3105 <muvvalapraneeth@gmail.com>
2026-08-13 19:00:13 +0530

    Nav Bar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { Button } from "@base-ui/react";
+import { ShoppingCart } from "lucide-react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const user = true;
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className="bg-orange-300 fixed top-0 w-full z-20 border-b border-gray-200">
+      <div className="max-w-7xl mx-auto flex justify-between items-center px-4 py-2">
+        {/* Logo Section */}
+        <div>
+          <img src="/Shop.png" alt="Shop Logo" className="w-[40px] h-auto" />
+        </div>
+        {/* {nav Section} */}
+        <nav className="flex gap-10 justify-between items-center">
+          <ul className="flex gap-7 items-center text-xl font-serif">
+            <Link to={"/"}>
+              <li>Home</li>
+            </Link>
+            <Link to={"/products"}>
+              <li>Products</li>
+            </Link>
+            {user && (
+              <Link to={"/profile"}>
+                <li>Hello User</li>
+              </Link>
+            )}
+          </ul>
+          <Link to={"/cart"} className="relative">
+            <ShoppingCart />
+            <span className="bg-red-500 rounded-full absolute text-white -top-3 -right-4 px-2">
+              0
+            </span>
+          </Link>
+          {user ? (
+            <Button className="bg-red-600 hover:bg-red-700 text-white rounded-md px-4 py-2 cursor-pointer font-serif shadow-sm">
+              Logout
+            </Button>
+          ) : (
+            <Button className="bg-gradient-to-r from-blue-600 to-purple-600 hover:from-blue-700 hover:to-purple-700 text-white rounded-md px-4 py-2 cursor-pointer font-serif shadow-sm">
+              Login
+            </Button>
+          )}
+        </nav>
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,170 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Eye, EyeOff, Loader2 } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+import axios from "axios";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
-export default Login
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+    try {
+      setLoading(true);
+      const res = await axios.post(
+        `http://localhost:8000/api/v1/user/login`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      );
+      if (res.data.success) {
+        navigate("/");
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <div className="flex justify-center items-center min-h-screen font-serif ">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="font-serif text-center">
+            Create your account
+          </CardTitle>
+          <CardDescription className="text-center">
+            Enter Your Details to Create Your Account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-col gap-3">
+            <div className="grid grid-cols-2 gap-4">
+              {/* <div className="grid gap-2">
+                <Label htmlFor="firstName">First Name</Label>
+                <Input
+                  id="firstName"
+                  name="firstName"
+                  type="text"
+                  placeholder="John"
+                  required
+                  value={formData.firstName}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="lastName">Last Name</Label>
+                <Input
+                  id="lastName"
+                  name="lastName"
+                  type="text"
+                  placeholder="Doe"
+                  required
+                  value={formData.lastName}
+                  onChange={handleChange}
+                />
+              </div> */}
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="example@gamil.com"
+                required
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+              </div>
+              <div className="relative">
+                <Input
+                  id="password"
+                  name="password"
+                  placeholder="Enter Your Password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  type={showPassword ? "text" : "password"}
+                  required
+                />
+                {showPassword ? (
+                  <EyeOff
+                    onClick={() => setShowPassword(false)}
+                    className="w-5 h-5 text-black-700 absolute right-4 bottom-2 "
+                  />
+                ) : (
+                  <Eye
+                    onClick={() => setShowPassword(true)}
+                    className="w-5 h-5 text-black-700 absolute right-4 bottom-2 "
+                  />
+                )}
+              </div>
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button
+            onClick={submitHandler}
+            type="submit"
+            className="w-full cursor-pointer bg-black hover:bg-gray-500"
+          >
+            {loading ? (
+              <>
+                <Loader2 className="h-4 w-4 animate-spin mr-2" />
+                Please Wait
+              </>
+            ) : (
+              "Login "
+            )}
+          </Button>
+          <p className="text-gray-700 text-sm">
+            Don't Have an Account?{" "}
+            <Link
+              to="/signup"
+              className="text-blue-700 hover:underline cursor-pointer"
+            >
+              Signup
+            </Link>
+          </p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+export default Login;
