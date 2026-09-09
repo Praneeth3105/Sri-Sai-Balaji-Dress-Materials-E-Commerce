b933907f495ea048fd09238d504f47b524cbce01
Praneeth3105 <muvvalapraneeth@gmail.com>
2026-09-09 14:09:40 +0530

    cr

--- backend/routes/contactRoute.js
+++ backend/routes/contactRoute.js
@@ -9,8 +9,6 @@ import { isAuthenticated, isAdmin } from "../middleware/isAuthenticated.js";
 
 const router = express.Router();
 
-// PUBLIC
-// Anyone can submit a contact message
 router.post("/", submitContact);
 
 router.get("/", isAuthenticated, isAdmin, getContactMessages);
